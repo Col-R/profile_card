3ba9aa35917a1f52c2cfb8a001c1ed3d9bf71a90
Col-R <2colewestin@gmail.com>
2022-08-17 16:50:05 -0500

    alignment, coloring, and background image

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,7 +3,7 @@ import './Avatar.css'
 
 const Avatar = () => {
   return (
-    <div>
+    <div className = "avatar__background">
         <img className = "avatar" src="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2Njc5NDYzOTQ4NDYxNDA4/michael-jordan.jpg" alt="avatar"  />
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import './Header.css'
 const Header = () => {
   return (
     <div className = "header">
-        <h1>Michael Jordan</h1>
-        <h4>Basketball Player</h4>
-        <h5>jordan.com</h5>
-        <button>Email</button>
+        <h1 className = "header__name">Michael Jordan</h1>
+        <h4 className = "header__job">Basketball Player</h4>
+        <h5 className = "header__website"><a href = "https://www.jordan.com/collection/">www.jordan.com/collection/</a></h5>
+        <button id = "header_email">Follow+</button>
     </div>
   )
 }
