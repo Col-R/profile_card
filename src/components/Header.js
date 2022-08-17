ef74ae6b039cbdab6b007c71346fafebafbb9894
Col-R <2colewestin@gmail.com>
2022-08-17 15:13:35 -0500

    alignment and spacing work

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div className = "main">
+    <div className = "header">
         <h1>Michael Jordan</h1>
         <h4>Basketball Player</h4>
         <h5>jordan.com</h5>
